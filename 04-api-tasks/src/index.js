e0b1a3b1c46a07bcce04b2f9b6a17701b81ff597
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2024-08-06 00:38:21 +0000

    feat(api-tasks): adiciona arquivo knexfile.js com configurações do Knex feat(api-tasks): adiciona script knex:migrate para executar migrações feat(api-tasks): atualiza script start no package.json para iniciar a aplicação feat(api-tasks): refatora conexão do Knex no arquivo index.js para utilizar configurações do knexfile.js feat(api-tasks): adiciona endpoint POST /tasks para criar uma nova tarefa feat(api-tasks): adiciona endpoint GET /tasks para obter todas as tarefas feat(api-tasks): adiciona endpoint GET /tasks/:id para obter uma tarefa específica feat(api-tasks): adiciona endpoint DELETE /tasks/:id para deletar uma tarefa feat(api-tasks): adiciona endpoint PUT /tasks/:id para atualizar uma tarefa feat(api-tasks): adiciona arquivo de migração para criar a tabela 'tasks' feat(api-tasks): adiciona README.md com referências sobre CRUD

--- 04-api-tasks/src/index.js
+++ 04-api-tasks/src/index.js
@@ -8,17 +8,10 @@
 
 import express from 'express'
 import knex from 'knex'
+import console from 'node:console'
+import knexConfig from '../knexfile.js'
 
-const db = knex({
-  client: 'mysql2',
-  connection: {
-    host: '0.0.0.0',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'tasks-api',
-  }
-})
+const db = knex(knexConfig)
 
 const app = express()
 
@@ -26,50 +19,112 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello, World!' })
-})
-
-app.post('/create-database-tables', async (req, res) => {
-  try {
-    await db.schema.createTable('tasks', (table) => {
-      table.increments()
-      table.string('name')
-      table.boolean('completed')
-      table.timestamps()
-    })
-
-    res.json({ message: 'Tables created successfully' })
-  } catch (error) {
-    console.error(error)
-    res.json({ message: 'Failed to create tables' })
-  }
-})
-
-app.get('/tasks', (req, res) => {
-  const tasks = [
-    { id: 1, name: 'Comprar mais leite', completed: false },
-    { id: 2, name: 'Pegar as crianças na escola', completed: true },
-  ]
-
-  res.json(tasks)
-})
-
 app.post('/tasks', async (req, res) => {
   try {
-    const data = req.body
-    console.info(data, 'capturando dados do body para iniciar o cadastro')
+    const { name, completed } = req.body
 
-    const [id] = await db.table('tasks').insert(data)
+    const data = {
+      name,
+      completed,
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+
+    const [id] = await db('tasks').insert(data)
 
     res
       .status(201)
-      .json({ id })
+      .json({ id, name, completed })
   } catch (error) {
     res
       .status(500)
-      .json({ message: 'Falhou ao cadastrar' })
+      .json({ error: 'Internal server error' })
   }
+})
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await db.select('*').from('tasks')
+
+  res
+    .status(200)
+    .json(tasks)
+})
+
+app.get('/tasks/:id', async (req, res) => {
+  const { id } = req.params
+
+  const task = await db.select('*')
+    .from('tasks')
+    .where({ id }).first()
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ error: 'Task not found' })
+  }
+
+  res
+    .status(200)
+    .json(task)
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  const { id } = req.params
+
+  // SELECT * FROM tasks WHERE id = ?
+  const task = await db.select('*')
+    .from('tasks')
+    .where({ id })
+    .first()
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ error: 'Task not found' })
+  }
+
+  // DELETE FROM tasks WHERE id = ?
+  await db('tasks')
+    .where({ id })
+    .delete()
+
+  res
+    .status(404)
+    .json({ message: 'Task deleted' })
+})
+
+app.put('/tasks/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, completed } = req.body
+
+  // SELECT * FROM tasks WHERE id = ?
+  const task = await db.select('*')
+    .from('tasks')
+    .where({ id })
+    .first()
+
+  // valida se a task existe, caso contrário retorna 404
+  if (!task) {
+    return res
+      .status(404)
+      .json({ error: 'Task not found' })
+  }
+
+  const data = {
+    name,
+    completed,
+    updated_at: new Date()
+  }
+
+  // UPDATE tasks SET name = ?, completed = ? WHERE id = ?
+  await db('tasks')
+    .where({ id })
+    .update(data)
+
+  // retorna a task atualizada
+  res
+    .status(200)
+    .json({ id, name, completed })
 })
 
 // starts a simple http server locally on port 3000
---
+++ README.md
@@ -0,0 +1,5 @@
+# Node training
+
+## Referências
+
+- [O que é CRUD](https://coodesh.com/blog/dicionario/o-que-e-crud)
